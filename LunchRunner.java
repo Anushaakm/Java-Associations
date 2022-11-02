2f696b1b3a888231e4d7b63f3cfb5d71a19ff612
Anusha KM <113164399+Anushaakm@users.noreply.github.com>
2022-11-02 22:32:00 +0530

    Encapsulation example1

---
+++ LunchRunner.java
@@ -0,0 +1,26 @@
+package com.xworkz.encapsulation.examples.boot;
+
+import com.xworkz.encapsulation.examples.things.Lunch;
+import com.xworkz.encapsulation.examples.things.Owner;
+
+public class LunchRunner {
+public static void main(String[] args) {
+	
+	Lunch lunch = new Lunch();
+	Owner owner=new Owner();
+	
+	owner.useLunchInfo(lunch);
+	System.out.println("Reading  private properties of another class(encapsulation)");
+	System.out.println(lunch.getHotelName());
+	System.out.println(lunch.getChat());
+	System.out.println(lunch.getDiscount());
+	System.out.println(lunch.getMainCourse());
+	System.out.println(lunch.getOrderedBy());
+	System.out.println(lunch.getOrderedOn());
+	System.out.println(lunch.getOrderNo());
+	System.out.println(lunch.getPrice());
+	System.out.println(lunch.getQuantity());
+	System.out.println(lunch.getSideDish());
+	System.out.println(lunch.getStarters());
+}
+}
